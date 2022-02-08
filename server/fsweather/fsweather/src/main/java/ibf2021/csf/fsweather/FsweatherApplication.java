b0c52eaa78f8d0ec40917331d238e68586e8664f
Shiraj <muhamadshiraj@gmail.com>
2022-02-08 13:46:38 +0800

    first commit

---
+++ server/fsweather/fsweather/src/main/java/ibf2021/csf/fsweather/FsweatherApplication.java
@@ -0,0 +1,13 @@
+package ibf2021.csf.fsweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsweatherApplication.class, args);
+	}
+
+}
---
+++ server/fsweather/fsweather/src/test/java/ibf2021/csf/fsweather/FsweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf2021.csf.fsweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
